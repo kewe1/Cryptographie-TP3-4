a7cf2ec6be3ba8e3dfba73e420c39a1841ac75c5
Kewe francois <francois.kewe@etu.unilim.fr>
2020-02-24 14:36:43 +0100

    Fonction Hachage

--- tp/Hachage.java
+++ tp/Hachage.java
@@ -9,24 +9,25 @@ public class Hachage {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Veuillez saisir votre mot de passe");
         String mdp = scanner.nextLine();
-        String sha1="";
+        String sha1 = "";
+        byte[] tab;
         try {
-            MessageDigest md = MessageDigest.getInstance("SHA-1");
-            md.reset();
-            md.update(mdp.getBytes("UTF-8"));
-            byte[] tab=md.digest();
-            System.out.println(tab);
-            sha1=String.format("%040x",new BigInteger(1, md.digest()));
-        } catch (NoSuchAlgorithmException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        } catch (UnsupportedEncodingException e) {
+            tab = Sha1(mdp, sha1);
+            sha1=String.format("%040x",new BigInteger(1,tab));
+        } catch (NoSuchAlgorithmException | UnsupportedEncodingException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
-        
         System.out.println("Voici votre mot de passe haché : " +sha1);
         scanner.close();
 
+    }
+
+    private static byte[] Sha1(String mdp, String sha1) throws NoSuchAlgorithmException, UnsupportedEncodingException {
+        MessageDigest md = MessageDigest.getInstance("SHA-1");
+        md.reset();
+        md.update(mdp.getBytes("UTF-8"));
+        byte[] tab=md.digest();
+        return tab;
     }
 }
