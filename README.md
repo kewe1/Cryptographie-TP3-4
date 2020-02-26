9c828770707e79133857c1bbcd3d22be16b1929d
Kewe francois <francois.kewe@etu.unilim.fr>
2020-02-26 14:45:58 +0100

    HMAC

--- README.md
+++ README.md
@@ -1,10 +1,15 @@
 # Cryptographie-TP3-4
 
 TP fait en java
-
+## Exercice 1 :
 1) Utilisation de la librairie java.security pour hacher un mot de passe
 
 2) Utilisation du fichier ods5.txt donnée dans le tp.
     Nous avons essayé 62 mots avec le mot: motdepasse
 
 3) 
+
+## Exercice 2 : 
+    On remarque que en utilisant la même clé et le même message en utilisant SHA1 puis SHA512 on obtient :
+        -Un résultat différent en octet et en hexadécimal
+        -Une longueur différente (en hexadécimal SHA1 : 40 caractères | SHA512 : 128 caractères ) 
--- tp/Hachage.java
+++ tp/Hachage.java
@@ -4,21 +4,51 @@ import java.math.BigInteger;
 import java.security.*;
 import java.util.Scanner;
 
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+
 public class Hachage {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Veuillez saisir votre mot de passe");
         String mdp = scanner.nextLine();
-        String sha1 = "";
         byte[] tab;
+
         try {
             tab = Sha1(mdp);
-            sha1 = String.format("%040x", new BigInteger(1, tab));
+            String sha1 = String.format("%040x", new BigInteger(1, tab));
+            System.out.println("Voici votre mot de passe haché : " + sha1);
         } catch (NoSuchAlgorithmException | UnsupportedEncodingException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
-        System.out.println("Voici votre mot de passe haché : " + sha1);
+        
+        System.out.println("Veuillez saisir une clé pour HMAC : ");
+        String key =scanner.nextLine();
+        System.out.println("Veuillez saisir un message :");
+        String msg=scanner.nextLine();
+        try {
+            byte[] res1=hmacSha1(key,msg);
+            byte[] res512=hmacSha512(key,msg);
+            String res1Byte="";
+            for (byte b : res1) {
+                res1Byte+=b+" ";
+            }
+            String res512Byte="";
+            for (byte b : res512) {
+                res512Byte+=b+" ";
+            }
+            System.out.println("Voici le HMAC SHA1 en chaine d'octet : "+res1Byte);
+            String hmacSha1=String.format("%040x", new BigInteger(1,res1));
+            System.out.println("Voici le HMAC SHA1 en hexadécimal : "+hmacSha1);
+            System.out.println(" ");
+            System.out.println("Voici le HMAC SHA512 en chaine d'octet : "+res512Byte);
+            String hmacSha512=String.format("%040x", new BigInteger(1,res512));
+            System.out.println("Voici le HMAC SHA512 en hexadécimal : "+hmacSha512);
+        } catch (NoSuchAlgorithmException | UnsupportedEncodingException | InvalidKeyException e) {
+            //TODO: handle exception
+            e.printStackTrace();
+        }
         scanner.close();
 
     }
@@ -29,5 +59,24 @@ public class Hachage {
         md.update(mdp.getBytes("UTF-8"));
         byte[] tab=md.digest();
         return tab;
+    }
+
+    public static byte[] hmacSha1(String key, String msg) throws NoSuchAlgorithmException, UnsupportedEncodingException,
+            InvalidKeyException {
+        byte[] keyByte=key.getBytes("UTF-8");
+        SecretKeySpec secretKey=new SecretKeySpec(keyByte,"HmacSHA1");
+        Mac mac= Mac.getInstance("HmacSHA1");
+        mac.init(secretKey);
+        return mac.doFinal(msg.getBytes("UTF-8"));
+        
+    }
+    public static byte[] hmacSha512(String key,String msg) throws NoSuchAlgorithmException, UnsupportedEncodingException,
+            InvalidKeyException {
+        byte[] keyByte=key.getBytes("UTF-8");
+        SecretKeySpec secretKey=new SecretKeySpec(keyByte,"HmacSHA512");
+        Mac mac= Mac.getInstance("HmacSHA512");
+        mac.init(secretKey);
+        return mac.doFinal(msg.getBytes("UTF-8"));
+        
     }
 }
