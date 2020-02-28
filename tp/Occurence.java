f60a28f871855def1af5a8e9aba59aed71b9b7bc
Ines <ines.chafiq@gmail.com>
2020-02-28 08:06:16 +0100

    occurence

--- tp/Occurence.java
+++ tp/Occurence.java
@@ -1,42 +1,63 @@
 import java.io.*;
 import java.security.NoSuchAlgorithmException;
+import java.util.HashSet;
 
 public class Occurence {
 
 	public static void main(String[] args) {
-		collision("motdepasse", 6);
+		collision(5);
 	}
 
-	public static void collision(String mdp, int N) {
+	public static void collision(int N) {
 		String thisLine = null;
+		HashSet set = new HashSet();
 		BufferedReader r;
-		int i = 0;
+		int i;
 		Hachage sha = new Hachage();
 		Hachage sha2 = new Hachage();
-		byte[] tab;
+		byte[] tab;	
+		int first_byte;	
+		int first_byte_dico;
+		boolean continu = true;
+		String mot;
 		try {
-			tab = sha.Sha1(mdp);
-			int first_byte = (tab[0] % (1<<N));
-			try {
 				r = new BufferedReader(new FileReader("./tp/dictionnaire.txt"));
-		
-				while ((thisLine = r.readLine()) != null) {
-					byte[] tab2 = sha2.Sha1(thisLine);
-					int first_byte_dico = (tab2[0] % (1<<5));
-	
-					if(first_byte == first_byte_dico){
-						System.out.println("Collision sur " + thisLine + "nb essaie " + i);
-						break;
+				
+				while ((thisLine = r.readLine()) != null && continu) {
+					tab = sha.Sha1(thisLine);
+					if (N < 7){
+						first_byte = (tab[0] % (1<<N));
 					}
-					i++;
+					else{
+						first_byte = (int) ((tab[0] * Math.pow(2,8) + tab[2]) % (1<<N));
+					}
+					set.add(first_byte);
+					mot = thisLine;
+					i = 0;
+					while ((thisLine = r.readLine()) != null){
+						byte[] tab2 = sha2.Sha1(thisLine);
+						if (N < 7){
+							 first_byte_dico = (tab2[0] % (1<<N));
+						}
+						else{
+							 first_byte_dico = (int) ((tab2[0] * Math.pow(2,8) + tab2[2]) % (1<<N));
+						}						
+						if(set.contains(first_byte_dico)){
+							System.out.println("Collision de " + mot +" sur " + thisLine + " nb essaie " + i);
+							continu = false;
+							break;
+						}						
+						i++;
+					}
 				}				
 			}
-			catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+		
+		catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
-		} catch (NoSuchAlgorithmException | UnsupportedEncodingException e1) {
+		catch (NoSuchAlgorithmException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
