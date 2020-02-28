e69368be3ef3d1a713b4710cf6c7a639b6f64745
Ines <ines.chafiq@gmail.com>
2020-02-28 09:26:36 +0100

    ajout au readme et finition

--- README.md
+++ README.md
@@ -5,9 +5,24 @@ TP fait en java
 1) Utilisation de la librairie java.security pour hacher un mot de passe
 
 2) Utilisation du fichier ods5.txt donnée dans le tp.
-    Nous avons essayé 62 mots avec le mot: motdepasse
+    Nous avons utilise une fonction a part qui trqnsforme les byte en bit et donne un type string, 
+    afin aue cela soit plus rapide.
+    Nous avons fait les tests avec le mot bonjour
+    Nombre essaies
+        pour 5 bits : 29
+        pour 6 bits : 45
+        pour 7 bits : 45
+        pour 8 bits : 45
+        pour 9 bits : 836
+        pour 10 bits : 1071
 
-3) 
+    La propriete des fonctions de hachage est la resistance aux collisions
+3) Nous avons trouve une collision sur le mot abaca au bout de 7 essaie avec 5 bits
+    6 bits: 7 avec le mot abaca
+    7 bits: 7 avec le mot abaca
+    8 bits: 172 avec le mot abatardis
+    9 bits: 172 avec le mot abatardis
+    10 bits: 172 avec le mot abatardis
 
 ## Exercice 2 : 
     On remarque que en utilisant la même clé et le même message en utilisant SHA1 puis SHA512 on obtient :
--- tp/Occurence.java
+++ tp/Occurence.java
@@ -5,51 +5,33 @@ import java.util.HashSet;
 public class Occurence {
 
 	public static void main(String[] args) {
-		collision(5);
+		collision(10, "poisson");
 	}
 
-	public static void collision(int N) {
+	public static void collision(int N, String mot) {
 		String thisLine = null;
-		HashSet set = new HashSet();
 		BufferedReader r;
 		int i;
 		Hachage sha = new Hachage();
 		Hachage sha2 = new Hachage();
 		byte[] tab;	
-		int first_byte;	
-		int first_byte_dico;
-		boolean continu = true;
-		String mot;
 		try {
 				r = new BufferedReader(new FileReader("./tp/dictionnaire.txt"));
 				
-				while ((thisLine = r.readLine()) != null && continu) {
-					tab = sha.Sha1(thisLine);
-					if (N < 7){
-						first_byte = (tab[0] % (1<<N));
-					}
-					else{
-						first_byte = (int) ((tab[0] * Math.pow(2,8) + tab[2]) % (1<<N));
-					}
-					set.add(first_byte);
-					mot = thisLine;
-					i = 0;
-					while ((thisLine = r.readLine()) != null){
-						byte[] tab2 = sha2.Sha1(thisLine);
-						if (N < 7){
-							 first_byte_dico = (tab2[0] % (1<<N));
-						}
-						else{
-							 first_byte_dico = (int) ((tab2[0] * Math.pow(2,8) + tab2[2]) % (1<<N));
-						}						
-						if(set.contains(first_byte_dico)){
-							System.out.println("Collision de " + mot +" sur " + thisLine + " nb essaie " + i);
-							continu = false;
-							break;
-						}						
-						i++;
-					}
-				}				
+				tab = sha.Sha1(mot);
+				String motchercher = conversionByteEnBit(tab, N);
+			
+				i = 1;
+				while ((thisLine = r.readLine()) != null){
+					byte[] tab2 = sha2.Sha1(thisLine);
+					String motdico = conversionByteEnBit(tab2, N);
+						
+					if(motchercher.equals(motdico) && !mot.equals(thisLine)){
+						System.out.println("Collision de " + mot +" sur " + thisLine + " nb essaie " + i);
+						break;
+					}						
+					i++;
+				}
 			}
 		
 		catch (IOException e) {
@@ -64,4 +46,12 @@ public class Occurence {
 
 	
 	}
+
+	private static String conversionByteEnBit(byte[] mot, int N) {
+        StringBuffer str = new StringBuffer();
+        for (byte bytes : mot) {
+            str.append(String.format("%8s", Integer.toBinaryString(bytes & 0xFF)).replace(' ', '0'));
+        }
+        return str.toString().substring(0, N);
+    }
 }
